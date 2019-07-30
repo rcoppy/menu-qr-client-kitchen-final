11946c0eb703b94e30fdc50f872d92c41d38af43
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-30 11:57:32 +0200

    inital setup

---
+++ src/views/OrdersIndex.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-container>
+      <h1>Kitchen orders</h1>
+
+      <ul>
+        <li v-for="order in orders" v-bind:key="order.id">{{ order.name }}</li>
+      </ul>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrdersIndex",
+  data() {
+    return {
+      orders: [
+        { id: 1, name: "Felix's order" },
+        { id: 2, name: "Charles's order" },
+        { id: 3, name: "Freddy's order" }
+      ]
+    };
+  }
+};
+</script>
